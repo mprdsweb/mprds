941999c4a195ea7b3565ae187d01d576cca55c6c
Jonathan De Leon <jon.deleon>
2016-07-16 00:36:40 -0700

    Changed html loader, scripts, and front page layout

--- includes/load_html.php
+++ includes/load_html.php
@@ -1,8 +1,7 @@
 <?php
 	if(!$_POST['page']) die("0");
 	$page = (string)$_POST['page'];
-	//if(file_exists('../static/html/'.$page.'.html'))
-	//	echo file_get_contents('../static/html/'.$page.'.html');
-	//else 
-	echo 'There is no such page!';
+	if(file_exists('../static/html/'.$page.'.html'))
+		echo file_get_contents('../static/html/'.$page.'.html');
+	else echo 'There is no such page!';
 ?>
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $.ajax({
             type: "GET",
             url: "includes/load_html.php",
-            //data: 'page=home',
+            data: 'page=home',
             dataType: "html",
             success: function(msg){
                 if(parseInt(msg)!=0){
@@ -17,29 +17,30 @@ $(document).ready(function() {
         var jsData = window.sessionStorage.getItem("main-content");
         $('#main-content').html(jsData);
     }
-});
-$('#navbar a').click(function() {
-    var $linkClicked = $(this).attr('href');
-    window.location.hash = $linkClicked;
-    var $pageRoot = $linkClicked.replace(/^#/, '');
-    if (!$(this).parent().hasClass("active")) {
-        $("#navbar a").parent().removeClass("active");
-        $(this).parent().addClass("active");
-        $.ajax({
-            type: "POST",
-            url: "includes/load_html.php",
-            data: 'page='+$pageRoot,
-            dataType: "html",
-            success: function(msg){
-                if(parseInt(msg)!=0){
-                    $('#main-content').html(msg);
-                    $('#main-content #page-content').hide().fadeIn();
-                    window.sessionStorage.setItem("main-content", msg); // store it in session
+
+    $('.nav a').click(function() {
+        var $linkClicked = $(this).attr('href');
+        window.location.hash = $linkClicked;
+        var $pageRoot = $linkClicked.replace(/^#/, '');
+        if (!$(this).parent().hasClass("active")) {
+            $(".nav a").parent().removeClass("active");
+            $(this).parent().addClass("active");
+            $.ajax({
+                type: "POST",
+                url: "includes/load_html.php",
+                data: 'page='+$pageRoot,
+                dataType: "html",
+                success: function(msg){
+                    if(parseInt(msg)!=0){
+                        $('#main-content').html(msg);
+                        //$('#main-content #page-content').hide().fadeIn();
+                        window.sessionStorage.setItem("main-content", msg); // store it in session
+                    }
                 }
-            }
-        });
-    }
-    else {
-        event.preventDefault();
-    }
+            });
+        }
+        else {
+            event.preventDefault();
+        }
+    });
 });
