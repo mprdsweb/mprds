3b7bb1d2cfe02f7115aa9264f8855a6449a6ab3c
Jonathan De Leon <jon.deleon>
2016-07-17 14:46:37 -0700

    changes to the home page, scripts, and css

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    console.log("Document ready function");
     if (window.sessionStorage.getItem("main-content") === null) {
-        console.log("No session storage");
         $.ajax({
             type: "POST",
             url: "includes/load_html.php",
@@ -9,7 +7,6 @@ $(document).ready(function() {
             dataType: "html",
             success: function(msg){
                 if(parseInt(msg)!=0){
-                    console.log(msg);
                     $('#main-content').html(msg);
                     //$('#main-content #page-content').hide().fadeIn();
                     window.sessionStorage.setItem("main-content", msg); // store it in session
@@ -21,14 +18,15 @@ $(document).ready(function() {
         $('#main-content').html(jsData);
     }
 
-    $('.nav a').click(function() {
+    $('nav a').click(function() {
+        console.log("clicked");
         var $linkClicked = $(this).attr('href');
         if(typeof $linkClicked !== typeof undefined && $linkClicked !== false){
             window.location.hash = $linkClicked;
             var $pageRoot = $linkClicked.replace(/^#/, '');
             if($pageRoot.length > 0){
                 if (!$(this).parent().hasClass("active")) {
-                    $(".nav a").parent().removeClass("active");
+                    $("nav a").parent().removeClass("active");
                     $(this).parent().addClass("active");
                     $.ajax({
                         type: "POST",
@@ -39,7 +37,6 @@ $(document).ready(function() {
                             if(parseInt(msg)!=0){
                                 $('#main-content').html(msg);
                                 //$('#main-content #page-content').hide().fadeIn();
-                                //window.sessionStorage.setItem("main-content", msg); // store it in session
                             }
                         }
                     });
