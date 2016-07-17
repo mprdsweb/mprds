28ced5b2c4c3e05736ef5563b2dc8e6479ca3e34
Jonathan De Leon <jon.deleon>
2016-07-16 17:53:39 -0700

    same as before

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,46 +1,53 @@
 $(document).ready(function() {
+    console.log("Document ready function");
     if (window.sessionStorage.getItem("main-content") === null) {
+        console.log("No session storage");
         $.ajax({
-            type: "GET",
+            type: "POST",
             url: "includes/load_html.php",
             data: 'page=home',
             dataType: "html",
             success: function(msg){
                 if(parseInt(msg)!=0){
+                    console.log(msg);
                     $('#main-content').html(msg);
                     //$('#main-content #page-content').hide().fadeIn();
                     window.sessionStorage.setItem("main-content", msg); // store it in session
                 }
             }
         });
-    } else {
+    }/* else {
         var jsData = window.sessionStorage.getItem("main-content");
         $('#main-content').html(jsData);
-    }
+    }*/
 
     $('.nav a').click(function() {
         var $linkClicked = $(this).attr('href');
-        window.location.hash = $linkClicked;
-        var $pageRoot = $linkClicked.replace(/^#/, '');
-        if (!$(this).parent().hasClass("active")) {
-            $(".nav a").parent().removeClass("active");
-            $(this).parent().addClass("active");
-            $.ajax({
-                type: "POST",
-                url: "includes/load_html.php",
-                data: 'page='+$pageRoot,
-                dataType: "html",
-                success: function(msg){
-                    if(parseInt(msg)!=0){
-                        $('#main-content').html(msg);
-                        //$('#main-content #page-content').hide().fadeIn();
-                        window.sessionStorage.setItem("main-content", msg); // store it in session
-                    }
+        if(typeof $linkClicked !== typeof undefined && $linkClicked !== false){
+            window.location.hash = $linkClicked;
+            var $pageRoot = $linkClicked.replace(/^#/, '');
+            if($pageRoot.length > 0){
+                if (!$(this).parent().hasClass("active")) {
+                    $(".nav a").parent().removeClass("active");
+                    $(this).parent().addClass("active");
+                    $.ajax({
+                        type: "POST",
+                        url: "includes/load_html.php",
+                        data: 'page='+$pageRoot,
+                        dataType: "html",
+                        success: function(msg){
+                            if(parseInt(msg)!=0){
+                                $('#main-content').html(msg);
+                                //$('#main-content #page-content').hide().fadeIn();
+                                //window.sessionStorage.setItem("main-content", msg); // store it in session
+                            }
+                        }
+                    });
                 }
-            });
-        }
-        else {
-            event.preventDefault();
+                else {
+                    event.preventDefault();
+                }
+            }
         }
     });
 });
