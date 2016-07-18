d8bd0dea63d83ca7c6d9e788f08919f57ec77806
Jonathan De Leon <jon.deleon>
2016-07-17 21:24:30 -0700

    modified navbar, added html and css files, fixed cache issues, added google maps, added media files, adjusted home.html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
-    if (window.sessionStorage.getItem("main-content") === null) {
+    if(window.location.hash === 0){
+        loader('home');
+    }else{
+        var $pageRoot = window.location.hash.replace(/^#/, '');
+        if($pageRoot.length > 0){
+            loader($pageRoot);
+        }
+    }
+    function loader(ref){
         $.ajax({
             type: "POST",
             url: "includes/load_html.php",
-            data: 'page=home',
+            data: 'page='+ref,
             dataType: "html",
             success: function(msg){
                 if(parseInt(msg)!=0){
                     $('#main-content').html(msg);
                     //$('#main-content #page-content').hide().fadeIn();
-                    window.sessionStorage.setItem("main-content", msg); // store it in session
                 }
             }
         });
-    } else {
-        var jsData = window.sessionStorage.getItem("main-content");
-        $('#main-content').html(jsData);
     }
-
     $('nav a').click(function() {
-        console.log("clicked");
         var $linkClicked = $(this).attr('href');
         if(typeof $linkClicked !== typeof undefined && $linkClicked !== false){
             window.location.hash = $linkClicked;
@@ -28,18 +30,7 @@ $(document).ready(function() {
                 if (!$(this).parent().hasClass("active")) {
                     $("nav a").parent().removeClass("active");
                     $(this).parent().addClass("active");
-                    $.ajax({
-                        type: "POST",
-                        url: "includes/load_html.php",
-                        data: 'page='+$pageRoot,
-                        dataType: "html",
-                        success: function(msg){
-                            if(parseInt(msg)!=0){
-                                $('#main-content').html(msg);
-                                //$('#main-content #page-content').hide().fadeIn();
-                            }
-                        }
-                    });
+                    loader($pageRoot);
                 }
                 else {
                     event.preventDefault();
