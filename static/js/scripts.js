e3eafca13fec08e096153958d54df995cebcea99
Jonathan De Leon <jon.deleon>
2016-07-25 19:47:58 -0700

    Removed unnessesary calendar files, updated directory structure, updates to the static load content, added calendar functionality

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,40 +1,48 @@
-$(document).ready(function() {
-    if(window.location.hash.length === 0){
+function loader(url){
+    url = url.replace(/^.*[\\/]/, ""); //replaces everything between '/' and '/' with empty string
+    $.ajax({
+        type: "POST",
+        url: "/includes/load_html.php",
+        data: 'page='+url,
+        dataType: "html",
+        success: function(msg){
+            if(parseInt(msg)!=0){
+                $('main').html(msg);
+                $('main').hide().fadeIn(200);
+            }
+        }
+    });
+}
+$(window).on('popstate', function() {
+    var $pageRoot = window.location.pathname.replace(/^.*[\\/]/, "");
+    if($pageRoot.length === 0)
+        $pageRoot = 'home';
+    loader($pageRoot);
+});
+
+$(function() {
+    /*if(window.location.hash.length === 0){
         loader('home');
     }else{
-        var $pageRoot = window.location.hash.replace(/^#/, '');
+        //var $pageRoot = window.location.hash.replace(/^#/, '');
+        var $pageRoot = window.location.pathname.replace(/^.*[\\/]/, "");
         if($pageRoot.length > 0){
             loader($pageRoot);
         }
-    }
-    function loader(href){
-        $.ajax({
-            type: "POST",
-            url: "includes/load_html.php",
-            data: 'page='+href,
-            dataType: "html",
-            success: function(msg){
-                if(parseInt(msg)!=0){
-                    $('#main-content').html(msg);
-                }
-            }
-        });
-    }
-    $('nav a').click(function() {
-        var $linkClicked = $(this).attr('href');
-        if(typeof $linkClicked !== typeof undefined && $linkClicked !== false){
-            window.location.hash = $linkClicked;
-            var $pageRoot = $linkClicked.replace(/^#/, '');
-            if($pageRoot.length > 0){
-                if (!$(this).parent().hasClass("active")) {
-                    $("nav a").parent().removeClass("active");
-                    $(this).parent().addClass("active");
-                    loader($pageRoot);
-                }
-                else {
-                    event.preventDefault();
-                }
-            }
+    }*/
+    $('[data-html]').each(function(i, div){
+        var url = div.getAttribute("data-html");
+        loader(url);
+    });
+
+    $('nav a').on("click", function(event) {
+        event.preventDefault();
+        var $linkClicked = $(this).attr("href").replace(/^#/, ""); //replaces every '#' with empty string
+        if($linkClicked.length > 0){
+            window.history.pushState(null, null, $linkClicked);     //manipulates the history
+            $("nav a").parent().removeClass("active");
+            $(this).parent().addClass("active");
+            loader($linkClicked);
         }
     });
 });
